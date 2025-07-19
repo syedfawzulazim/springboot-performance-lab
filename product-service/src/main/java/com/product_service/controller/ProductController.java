a0c8ffa9814bb60a2bd7794533aedfbbb79f3001
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-07-19 17:32:08 +0200

    feat-01: get & post apis for product + docker compose

---
+++ product-service/src/main/java/com/product_service/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.product_service.controller;
+
+import com.product_service.Service.ProductService;
+import com.product_service.dto.ProductRequest;
+import com.product_service.dto.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse create(@RequestBody ProductRequest productRequest){
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+}
---
+++ product-service/src/main/java/com/product_service/dto/ProductRequest.java
@@ -0,0 +1,11 @@
+package com.product_service.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
+}
