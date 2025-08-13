87a706bb1270c8f3d8f8350e1d976ec6f754b626
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-08-13 02:21:04 +0200

    chore: small things

--- product-service/src/main/java/com/product_service/dto/ProductRequest.java
+++ product-service/src/main/java/com/product_service/dto/ProductRequest.java
@@ -1,6 +1,9 @@
 package com.product_service.dto;
 
+import org.springframework.util.Assert;
+
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record ProductRequest(
         String id,
